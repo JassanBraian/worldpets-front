023aa5e1821b8718d5b7086467d1a521a977a172
Braian <braianjassan@gmail.com>
2022-06-12 19:15:01 -0300

    folders were created

---
+++ src/components/common/navbar/NavBarComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBarComp = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NavBarComp;
---
+++ src/components/common/offcanvas/OffCanvasComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OffCanvasComp = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default OffCanvasComp;
