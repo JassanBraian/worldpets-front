c636867d398a3da7baf7b45ab7a0eb6eb8ecb6bf
lucianocolin <lucianocolin99@gmail.com>
2022-06-25 17:57:25 -0300

    Creating modal

--- src/components/entities/publication/base/ModCreatePublication.jsx
+++ src/components/entities/publication/base/ModCreatePublication.jsx
@@ -4,6 +4,19 @@ import '../../../../css/entities/admin/ModCreatePublication.css';
 import { useState } from 'react';
 
 const ModCreatePublication = (props) => {
+
+  const [ title, setTitle ] = useState('');
+  const [ description, setDescription ] = useState('');
+
+  const handeChange = e => {
+    setTitle(e.target.value)
+  }
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    setTitle('');
+  }
+
   return (
     <Modal
     {...props}
@@ -16,40 +29,48 @@ const ModCreatePublication = (props) => {
         Add Publication
       </Modal.Title>
     </Modal.Header>
-    <Modal.Body className='d-flex flex-column'>
-      <form>
-        <label htmlFor="title">Title</label>
-          <input type="text" 
-          name="tit" 
-          id="tit"
-          placeholder="Insert publication title..." />
-        <label htmlFor="desc">Description</label>
-          <textarea name="desc" 
-          id="desc" 
-          cols="10" 
-          rows="8"
-          placeholder="What's your publication about..."></textarea>
-        <label htmlFor="ubi">Ubication</label>
-          <input type="text" 
-          name="ubi" 
-          id='ubi'
-          placeholder="Where are you publishing from?"/>
-        <label htmlFor="cat">Category</label>
-          <select name="cat" id="cat">
-            <option value="lost">Lost</option>
-            <option value="found">Found</option>
-            <option value="adopt">Adoption</option>
-          </select>
-        <label htmlFor="user">User</label>
-          <input type="text" 
-          name="user" 
-          id="user" 
-          placeholder="Your username"/>
+    <Modal.Body>
+      <form className='d-flex flex-column'>
+      
+          <label htmlFor="title">Title</label>
+            <input type="text" 
+            name="tit" 
+            id="tit"
+            placeholder="Insert publication title..." 
+            value={title}
+            onChange={handeChange} />
+        
+          <label htmlFor="desc">Description</label>
+            <textarea name="desc" 
+            id="desc" 
+            cols="10" 
+            rows="8"
+            placeholder="What's your publication about..."></textarea>
+        
+          <label htmlFor="ubi">Ubication</label>
+            <input type="text" 
+            name="ubi" 
+            id='ubi'
+            placeholder="Where are you publishing from?"/>
+       
+          <label htmlFor="cat">Category</label>
+            <select name="cat" id="cat">
+              <option value="lost">Lost</option>
+              <option value="found">Found</option>
+              <option value="adopt">Adoption</option>
+            </select>
+        
+          <label htmlFor="user">User</label>
+            <input type="text" 
+            name="user" 
+            id="user" 
+            placeholder="Your username"/>
+
       </form>
     </Modal.Body>
     <Modal.Footer>
       <Button size='sm' onClick={props.onHide}>Close</Button>
-      <Button size='sm' type='submit'>Submit</Button>
+      <Button size='sm' type='submit' onSubmit={handleSubmit}>Submit</Button>
     </Modal.Footer>
   </Modal>
   )
