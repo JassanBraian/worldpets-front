bf1b78edebbc248e151169b07cabd86958f0d7bf
ElisaSoco <esocolsky@gmail.com>
2022-06-28 19:31:31 -0300

    primer commit desarrollon front card-categorias-home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Card from './components/entities/publication/cards/Card';
+import './css/common/layout/layout.css';
+import Category from './pages/Category';
+import Home from './pages/Home';
+
+
 function App() {
   return (
-    <>
-    </>
+    <Home />
   );
 }
 
