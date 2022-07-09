94798a4eae72fa59decfc47e35d3223aef156043
lucianocolin <lucianocolin99@gmail.com>
2022-07-09 10:49:29 -0300

    useEffect without use deleted

--- src/context/user/UserProvider.jsx
+++ src/context/user/UserProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import UserContext from './UserContext';
 import clientAxios from "../../config/axios";
 
