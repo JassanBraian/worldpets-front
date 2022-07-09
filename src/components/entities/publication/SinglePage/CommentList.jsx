ebc501894beda4c3c4a7af8c2a9768114fd89068
Braian <braianjassan@gmail.com>
2022-07-09 20:06:16 -0300

    'comments_by_publiId_running'

--- src/components/entities/publication/SinglePage/CommentList.jsx
+++ src/components/entities/publication/SinglePage/CommentList.jsx
@@ -1,28 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import { useState } from 'react';
 import {
-    getComments as getCommentsApi, 
+    getComments as getCommentsApi,
     createComment as createCommentApi,
     deleteComment as deleteCommentApi,
     updateComment as updateCommentApi
 } from './api';
 import CommentItem from './CommentItem';
 import CommentForm from './CommentForm';
+import CommentContext from '../../../../context/comment/CommentContext';
+import PublicationContext from '../../../../context/publication/PublicationContext';
 
-const CommentList = ({currentUserId}) => { /* currentUserId proviene desde el padre */
+const CommentList = () => {
+
+    const { commentsPubli, getCommentsByPubliId } = useContext(CommentContext);
+    const { publication } = useContext(PublicationContext);
+
+    useEffect(() => {
+        console.log(publication._id)
+        getCommentsByPubliId(publication._id);
+    }, [publication._id])
+
+    useEffect(() => {
+        console.log(commentsPubli);
+    }, [commentsPubli])
+
+
     const [backendComments, setBackendComments] = useState([]) /* Colocamos un array vacio porque no tenemos ninguna data y esta data tendria que venir desde el backend */
     const [activeComment, setActiveComment] = useState(null) /* Escuchara a dos objetos diferentes cuando tengamos un activeComment:
         {type: 'editing', id: '1'}
         {type: 'replying', id: '1'}
         La id nos indica en que comentario estaremos editando o respondiendo. Por eso es que agregaremos activeComment y setActiveComment en el componente Comment y tambien el el reply
     */
-    
+
     /* Mucho de los comentarios son replys por eso creo rootComments para traer a los comentarios padres*/
     const rootComments = backendComments.filter(backendComments => backendComments.parentId === null); /* Con esto traigo a los comentarios que no son replys, sino que son los comentarios padres */
-    const getReplies = commendId =>  { /* commendId es el identificador unico del comment padre. Asi es como podemos traer los replies comments del commentario, desde el backend */
+    const getReplies = commendId => { /* commendId es el identificador unico del comment padre. Asi es como podemos traer los replies comments del commentario, desde el backend */
         return backendComments.filter(backendComment => backendComment.parentId === commendId) /* Con esto traemos a los replies */
             .sort(  /* Todo esto lo hacemos para colocar las respuestas de modo que la mas nueva quede abajo */
-                (a,b) =>
+                (a, b) =>
                     new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
     };
 
@@ -31,10 +47,10 @@ const CommentList = ({currentUserId}) => { /* currentUserId proviene desde el pa
             setBackendComments([comment, ...backendComments])
             setActiveComment(null)
         })
-    } 
+    }
 
     const deleteComment = (commentId) => {
-        deleteCommentApi(commentId).then(()=> {
+        deleteCommentApi(commentId).then(() => {
             const updatedBackendComments = backendComments.filter(
                 backendComments => backendComments.id !== commentId
             );
@@ -46,7 +62,7 @@ const CommentList = ({currentUserId}) => { /* currentUserId proviene desde el pa
         updateCommentApi(text, commentId).then(() => {
             const updatedBackendComments = backendComments.map(backendComment => {
                 if (backendComment.id === commentId) {
-                    return {...backendComment, body: text}
+                    return { ...backendComment, body: text }
                 }
                 return backendComment
             })
@@ -61,30 +77,29 @@ const CommentList = ({currentUserId}) => { /* currentUserId proviene desde el pa
         })
     }, [])
 
-  return (
-    <div className='comments'>
-        <h3 className='comments-title'>Comentarios</h3>
-        <div className='comments-form-container'>
-        <div className="comment-form-title">Dejanos tu comentario</div>
-        <CommentForm submitLabel='Enviar' handleSubmit={addComment} />
+    return (
+        <div className='comments'>
+            <h3 className='comments-title'>Comentarios</h3>
+            <div className='comments-form-container'>
+                <div className="comment-form-title">Dejanos tu comentario</div>
+                <CommentForm submitLabel='Enviar' handleSubmit={addComment} />
+            </div>
+            <div className="comments-container">
+                {commentsPubli.map((comment, index) => (  /* Con esto hago un .map para traer a los comentarios padres que indique en rootComments */
+                    <CommentItem
+                        key={index + 1}
+                        comment={comment}
+                        replies={getReplies(comment.id)} /* Con esto recolecto todos los comment replies cuando se renderiza el comment */
+                        deleteComment={deleteComment}
+                        updateComment={updateComment}
+                        activeComment={activeComment}
+                        setActiveComment={setActiveComment}
+                        addComment={addComment}
+                    /> /* Con esto inserto Comment e indico a comment un rootComment porque tengo que acceder a todo el rootComment por completo. Al colocar comment={rootComment}, le estoy pasando la info de rootComment al elemento hijo Comment */
+                ))}
+            </div>
         </div>
-        <div className="comments-container">
-            {rootComments.map(rootComment => (  /* Con esto hago un .map para traer a los comentarios padres que indique en rootComments */
-                <CommentItem 
-                    key={rootComment.id} 
-                    comment={rootComment} 
-                    replies={getReplies(rootComment.id)} /* Con esto recolecto todos los comment replies cuando se renderiza el comment */
-                    currentUserId={currentUserId} /* Para que cada uno de los comentarios tenga un currentUserId */
-                    deleteComment={deleteComment}
-                    updateComment={updateComment}
-                    activeComment={activeComment}
-                    setActiveComment={setActiveComment}
-                    addComment={addComment}
-                /> /* Con esto inserto Comment e indico a comment un rootComment porque tengo que acceder a todo el rootComment por completo. Al colocar comment={rootComment}, le estoy pasando la info de rootComment al elemento hijo Comment */
-            ))}
-        </div>
-    </div>
-  )
+    )
 }
 
 export default CommentList;
